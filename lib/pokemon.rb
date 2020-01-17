3cc8a62cc07963b457aca4a3c10d760bbca3dee3
Shayla White <shayladwhite1@gmail.com>
2020-01-17 03:31:45 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -9,14 +9,26 @@ class Pokemon
     @db = db  
   end
 
-  def save
+ def self.save(name,type,db)
     sql = <<-SQL
-      INSERT INTO songs (name, album)
+      INSERT INTO pokemon (name, type) 
       VALUES (?, ?)
     SQL
  
-    DB[:conn].execute(sql, self.name, self.album)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+    db.execute(sql, name, type)
+    @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
   end
   
+  def self.find(id, db)
+    sql = <<-SQL
+      SELECT *
+      FROM pokemon
+      WHERE id = ?
+      LIMIT 1
+    SQL
+ 
+    db.execute(sql, id).map do |row|
+      pokemon = self.new(id:row[0],name:row[1],type:row[2], db:row[3])  
+    end.first
+  end
 end
